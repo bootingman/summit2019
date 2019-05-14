f2b03c0a1138b9407961b766a8c09313f41d1702
Federico Gimenez <federico.gimenez@gmail.com>
2019-05-14 15:27:35 +0200

    deployment and integration test scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ENVIRONMENT=${1:-staging}
+TAG=
+
+if [ "$ENVIRONMENT" = "production" ]; then
+   TAG="${CIRCLE_TAG}"
+else
+    TAG="${CIRCLE_SHA1}"
+fi
+
+/scripts/deploy.sh helm \
+                   --set image.tag="${TAG}" \
+                   --set environment="${ENVIRONMENT}" \
+                   summit2019 \
+                   ./charts/summit2019
---
+++ scripts/integration-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source /scripts/common.sh
+source /scripts/bootstrap-helm.sh
+
+
+run_tests() {
+    echo Running tests...
+
+    wait_pod_ready summit2019
+}
+
+teardown() {
+    helm delete --purge summit2019
+}
+
+main(){
+    if [ -z "$KEEP_W3F_SUMMIT2019" ]; then
+        trap teardown EXIT
+    fi
+
+    /scripts/build-helm.sh \
+        --set environment=ci \
+        --set image.tag="${CIRCLE_SHA1}" \
+        summit2019 \
+        ./charts/summit2019
+
+    run_tests
+}
+
+main
