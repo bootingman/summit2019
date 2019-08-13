79a43de16abb1e1cee65a83e686d257e4ddb82be
rmnprkrl <rmnprkrl@gmail.com>
2019-08-13 10:59:26 +0200

    tickets-soldout template content update

--- web/app/themes/web3s/templates/tickets-soldout.php
+++ web/app/themes/web3s/templates/tickets-soldout.php
@@ -50,5 +50,55 @@
             <div class="pretix-widget-clear"></div>
          </div>
       </div>
+      <div class="pretix-widget-item">
+         <div class="pretix-widget-item-row pretix-widget-main-item-row">
+            <div class="pretix-widget-item-info-col">
+               <div class="pretix-widget-item-title-and-description">
+                  <strong class="pretix-widget-item-title">Layer 2 - Early Adopter Rate - 3 Day Pass (Monday/Tuesday/Wednesday)</strong>
+                  <div class="pretix-widget-item-description">
+                     <p><strong>LIMITED AVAILABILITY</strong> -  This is a subsidized ticket for developers, builders, and advocates of the decentralized web, offered at a discounted price for a limited time only. Ticket includes admission to all three days of Web3 Summit 2019.</p>
+                  </div>
+               </div>
+            </div>
+            <div class="pretix-widget-item-price-col">
+               <div class="pretix-widget-pricebox">
+                  <span>EUR 299.00</span><small class="pretix-widget-pricebox-tax">incl. 19% MwSt.</small>
+               </div>
+            </div>
+            <div class="pretix-widget-item-availability-col">
+               <div class="pretix-widget-availability-box">
+                  <div class="pretix-widget-availability-available">
+                     <div class="pretix-soldout"><strong>Sold Out</strong></div>
+                  </div>
+               </div>
+            </div>
+            <div class="pretix-widget-clear"></div>
+         </div>
+      </div>
+      <div class="pretix-widget-item">
+         <div class="pretix-widget-item-row pretix-widget-main-item-row">
+            <div class="pretix-widget-item-info-col">
+               <div class="pretix-widget-item-title-and-description">
+                  <strong class="pretix-widget-item-title">Layer 3 - Early Adopter Rate - 3 Day Pass (Monday/Tuesday/Wednesday)</strong>
+                  <div class="pretix-widget-item-description">
+                     <p><strong>LIMITED AVAILABILITY</strong> -  This is a subsidized ticket for developers, builders, and advocates of the decentralized web, offered at a discounted price for a limited time only. Ticket includes admission to all three days of Web3 Summit 2019.</p>
+                  </div>
+               </div>
+            </div>
+            <div class="pretix-widget-item-price-col">
+               <div class="pretix-widget-pricebox">
+                  <span>EUR 349.00</span><small class="pretix-widget-pricebox-tax">incl. 19% MwSt.</small>
+               </div>
+            </div>
+            <div class="pretix-widget-item-availability-col">
+               <div class="pretix-widget-availability-box">
+                  <div class="pretix-widget-availability-available">
+                     <div class="pretix-soldout"><strong>Sold Out</strong></div>
+                  </div>
+               </div>
+            </div>
+            <div class="pretix-widget-clear"></div>
+         </div>
+      </div>
    </div>
 </div>
